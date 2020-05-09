424d18ee1b0e1af9b27f5f1b1e060beb4478a345
akassab <andrewkassab@live.com>
2020-05-08 17:09:55 -0700

    added extentions, reorganized project to modules

---
+++ tests/src/main/java/BaseWebTest.java
@@ -0,0 +1,47 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
+
+public class BaseWebTest {
+
+    // Path to all extensions we want in our test browser
+    private String[] EXTENSION_PATHS = new String[] {"INSERT_VELOCITY_RAPTOR_PATH_HERE",
+        "INSERT_CHROPATH_PATH_HERE"};
+
+    @BeforeMethod()
+    private void startBrowser(){
+        ChromeOptions chromeOptions = getChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        setWebDriver(driver);
+    }
+
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        List<File> extensionFiles = new ArrayList<File>();
+        Arrays.stream(EXTENSION_PATHS).forEach(path -> extensionFiles.add(new File(path)));
+        options.addExtensions(extensionFiles);
+        return options;
+    }
+
+    @AfterMethod(alwaysRun = true)
+    private void closeBrowser(){
+        getWebDriver().quit();
+    }
+
+}
