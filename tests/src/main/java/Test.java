7a6c193d56b0412b9ac4b4af6c3f65ab0dc0e9cb
akassab <andrewkassab@live.com>
2020-05-11 21:14:26 -0700

    some commit

---
+++ tests/src/main/java/Test.java
@@ -0,0 +1,4 @@
+public class Test {
+    // blah blah:w
+
+}
--- ui/src/main/java/ExtensionPage.java
+++ ui/src/main/java/ExtensionPage.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selenide.$x;
 
 // This is the main Page Object for our extention
 public class ExtensionPage extends BasePage {
-
+sfsdidsfgdsfg
     private final SelenideElement extensionHeader = $x("");
 
     public ExtensionPage() {
