208e0f6bcbfd925547f0cd3606384a881fd92bfb
akassab <andrewkassab@live.com>
2020-05-20 19:07:48 -0700

    fixed style

--- tests/src/main/java/SettingsNavigationTest.java
+++ tests/src/main/java/SettingsNavigationTest.java
@@ -5,12 +5,12 @@ public class SettingsNavigationTest extends BaseWebTest {
     @Test
     public void testNavigateSettings(){
         // verifications are automatically done in constructors, so no need to assert here
-            extensionPage.openSettingsPage()
-                .openVelocityRaptorSettingsPage()
-                .openMyTeamSettingsPage()
-                .openGithubRepoSettings()
-                .openGeneralSettingsPage()
-                .backToExtensionPage();
+        extensionPage.openSettingsPage()
+            .openVelocityRaptorSettingsPage()
+            .openMyTeamSettingsPage()
+            .openGithubRepoSettings()
+            .openGeneralSettingsPage()
+            .backToExtensionPage();
     }
 
 }
