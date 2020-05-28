4470626461bfa29bf4c9e58e39fddb4129d09a77
akassab <andrewkassab@live.com>
2020-05-28 13:29:58 -0700

    updated accessory names for typos

--- tests/src/main/java/ChangeAccessoriesTest.java
+++ tests/src/main/java/ChangeAccessoriesTest.java
@@ -8,7 +8,7 @@ public class ChangeAccessoriesTest extends BaseWebTest {
     VelocityRaptorSettingsPage raptorSettingsPage;
 
     @BeforeMethod
-    public void initTestData(){
+    public void initTestData() {
         raptorSettingsPage = extensionPage.openSettingsPage()
             .openVelocityRaptorSettingsPage();
     }
@@ -26,7 +26,7 @@ public class ChangeAccessoriesTest extends BaseWebTest {
         extensionPage = raptorSettingsPage.setAccessoryType(AccessoryTypes.BACK)
             .selectAccessoryByName(AccessoryNames.BACK_ANGEL_WING)
             .backToExtensionPage();
-        assertTrue(extensionPage.isRaptorWearingAccessory("Back Angel Wing Accesory"));
+        assertTrue(extensionPage.isRaptorWearingAccessory(AccessoryNames.BACK_ANGEL_WING));
     }
 
 
@@ -35,7 +35,7 @@ public class ChangeAccessoriesTest extends BaseWebTest {
         extensionPage = raptorSettingsPage.setAccessoryType(AccessoryTypes.TAIL)
             .selectAccessoryByName(AccessoryNames.SPIKED_ORANGE_TAIL)
             .backToExtensionPage();
-        assertTrue(extensionPage.isRaptorWearingAccessory("Spiked Orange Tail Accesory"));
+        assertTrue(extensionPage.isRaptorWearingAccessory(AccessoryNames.SPIKED_ORANGE_TAIL));
     }
 
     @Test
@@ -43,7 +43,7 @@ public class ChangeAccessoriesTest extends BaseWebTest {
         extensionPage = raptorSettingsPage.setAccessoryType(AccessoryTypes.HAND)
             .selectAccessoryByName(AccessoryNames.SABER)
             .backToExtensionPage();
-        assertTrue(extensionPage.isRaptorWearingAccessory("Saber Accesory"));
+        assertTrue(extensionPage.isRaptorWearingAccessory(AccessoryNames.SABER));
     }
 
 }
