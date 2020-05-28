fae7e2f58d5480688a61e2e1c080df7385f1521f
akassab <andrewkassab@live.com>
2020-05-28 14:40:14 -0700

    updated extension

--- ui/src/main/java/BasePage.java
+++ ui/src/main/java/BasePage.java
@@ -2,8 +2,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public abstract class BasePage {
 
-    // TODO: Change to VelocityRaptor later
-    private static final String extensionLink = "chrome-extension://bcpdbeigkofnegjinbifbjnliefpdaec/root/html/setup/auth.html";
+    private static final String extensionLink = "chrome-extension://eclgjepefdmkdlecfdehjmbdegoiokfm/root/html/setup/auth.html";
 
     public abstract void verifyIsOpened();
 
