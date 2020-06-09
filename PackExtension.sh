b35929e8ec603bc7f7e00b99ab170f794a1b2e92
Rahul Verma <rahul.j.verma@gmail.com>
2020-06-08 19:32:22 -0700

    Changed pack extension script target dir

--- PackExtension.sh
+++ PackExtension.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-resource_path="/ui/main/resources/extensions/velocity_raptor.crx"
+resource_path="./ui/src/main/resources/extensions/*.crx"
 extension_path="./../main"
 extension_key="CalamariCrew.pem"
 chrome.exe --pack-extension=[$extension_path] --pack-extension-key=[$extension_key]
 rm resource_path
-mv CalamariCrew.crx $resource_path
+mv CalamariCrew.crx ./ui/src/main/resources/extensions
