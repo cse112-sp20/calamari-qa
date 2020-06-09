8f83c12a5b1e0c87b6b2bce5b3c66d889c09dcc3
Rahul Verma <rahul.j.verma@gmail.com>
2020-06-08 18:24:51 -0700

    Added extension path

--- PackExtension.sh
+++ PackExtension.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 resource_path="/ui/main/resources/extensions/velocity_raptor.crx"
 # TODO: Here extension path should be root folder of Velocity Raptor extension.
-extension_path=""
+extension_path="./../main"
 extension_key="CalamariCrew.pem"
 chrome.exe --pack-extension=[$extension_path] --pack-extension-key=[$extension_key]
 rm resource_path
