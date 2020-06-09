2178db5e4a63bb14cf4fe880094fdb9542e8d76d
Rahul Verma <rahul.j.verma@gmail.com>
2020-06-08 19:09:27 -0700

    Get wget working

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -ex
-sh -c "wget -nv $*"
-wget https:://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sh -c "wget -nv dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install ./google-chrome-stable_current_amd64.deb
