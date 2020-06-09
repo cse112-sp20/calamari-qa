44b4e4761575b85b74d5d5558c14141f6e07f793
Rahul Verma <rahul.j.verma@gmail.com>
2020-06-08 18:56:13 -0700

    Remove windows ^M

--- PackExtension.sh
+++ PackExtension.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 resource_path="/ui/main/resources/extensions/velocity_raptor.crx"
-# TODO: Here extension path should be root folder of Velocity Raptor extension.
 extension_path="./../main"
 extension_key="CalamariCrew.pem"
 chrome.exe --pack-extension=[$extension_path] --pack-extension-key=[$extension_key]
