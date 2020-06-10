f0341eb823caf6533899ac3840afed5536a89373
Rahul Verma <rahul.j.verma@gmail.com>
2020-06-10 16:30:53 -0700

    Put path to main.js

--- InsertToken.sh
+++ InsertToken.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-FILE="PATH TO Main.js"
+#!/bin/bash
+FILE="./../main/root/src/js/main.js"
 sed -i -e "s/interactiveSignIn;/localStorage.setItem('token','9fbd7ff7a891faa714ef781fce59dd118433fabd');/g" $FILE
