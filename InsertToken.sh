268a1ddc820c14d84dbac0f10aec1ae31b11e923
akassab <andrewkassab@live.com>
2020-06-10 16:55:35 -0700

    token updated

--- InsertToken.sh
+++ InsertToken.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 FILE="./../main/root/src/js/main.js"
-sed -i -e "s/interactiveSignIn;/localStorage.setItem('token','c854140ea88243c5f66168c73bea5ac29fea91b8');/g" $FILE
+sed -i -e "s/interactiveSignIn;/localStorage.setItem('token','a7860421a05b85ebae92c2d1dd14601217f016bd');/g" $FILE
