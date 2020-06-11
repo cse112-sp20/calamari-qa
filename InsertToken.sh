1cfc7ff52dd8cbff4ff81c0f4485713406d22aca
akassab <andrewkassab@live.com>
2020-06-10 17:00:45 -0700

    github token as env variable

--- InsertToken.sh
+++ InsertToken.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 FILE="./../main/root/src/js/main.js"
-sed -i -e "s/interactiveSignIn;/localStorage.setItem('token','a7860421a05b85ebae92c2d1dd14601217f016bd');/g" $FILE
+sed -i -e "s/interactiveSignIn;/localStorage.setItem('token','${GITHUB_TOKEN}');/g" $FILE
